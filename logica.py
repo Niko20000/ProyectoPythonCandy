c742bd5097b570846aca22f8fb971a8244aa9554
MatiasNaranjoDesarrollador <maricuu121@gmail.com>
2026-03-09 15:58:26 -0500

    Corregi un problemita en el stock

--- logica.py
+++ logica.py
@@ -1,32 +1,53 @@
 def calcular_precio_dulceria():
     
     inventario = {
-        "bombones": {"precio": 12000, "paquete": 12 "cantidad" :100},
-        "chocolates": {"precio": 15000, "paquete": 30 "cantidad" :100},
-        "papas": {"precio": 20000, "paquete": 12 "cantidad" :100}
+        "bombones": {"precio": 12000, "cantidad": 100},
+        "chocolates": {"precio": 15000, "cantidad": 100},
+        "papas": {"precio": 20000, "cantidad": 100}
     }
 
-    print("=== Calculadora de Dulcería ===")
     total_compra = 0
+    
+    while True:
+        print("\n---------------- Menú de Dulcería ----------------")
+    
+        for producto, datos in inventario.items():
+            print(f"- {producto.capitalize()}: ${datos['precio']:,} (Stock: {datos['cantidad']})")
+        
+        print(" Salir (Escribe 'salir' para terminar y pagar)")
+        
 
-    for producto, datos in inventario.items():
-        precio = datos["precio"]
-        stock = datos["stock"]
+        eleccion = input("\n¿Qué producto desea comprar?: ").lower().strip()
 
-        print(f"\nProducto: {producto.capitalize()}")
-        print(f"Precio: ${precio:,}")
-        print(f"Stock disponible: {stock}")
+        if eleccion == "salir":
+            break
 
-        cantidad = int(input("¿Cuántas unidades desea comprar? "))
+        if eleccion in inventario:
+            try:
+                cantidad_pedida = int(input(f"¿Cuántas unidades de {eleccion} llevarás?: "))
+                
+                stock_actual = inventario[eleccion]["cantidad"]
+                precio_unitario = inventario[eleccion]["precio"]
 
-        subtotal = cantidad * precio
-        total_compra += subtotal
+                if cantidad_pedida <= stock_actual:
+                    subtotal = cantidad_pedida * precio_unitario
+                    total_compra += subtotal
+                    
+      
+                    inventario[eleccion]["cantidad"] -= cantidad_pedida
+                    
+                    print(f" Agregado: {cantidad_pedida} {eleccion} por ${subtotal:,}")
+                else:
+                    print(f" No hay suficiente stock. Solo quedan {stock_actual}.")
+            
+            except ValueError:
+                print(" Error: Ingresa un número válido para la cantidad.")
+        else:
+            print(" Ese producto no existe en nuestra tienda. Intenta de nuevo.")
 
-        print(f"Subtotal de {producto}: ${subtotal:,}")
-
-    print("\n" + "-" * 30)
+    print("\n" + "=" * 30)
+    print(f"RESUMEN DE COMPRA")
     print(f"TOTAL A PAGAR: ${total_compra:,}")
-    print("-" * 30)
-
+    print("=" * 30 + "\n¡Gracias por su compra!")
 
 calcular_precio_dulceria()
